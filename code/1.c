8fc1cd879947216e1fb42fcf80bc43ff54a8f27f
Chesakov Maxim <chesmax2005@gmail.com>
2026-07-06 00:19:01 +0400

    .

--- code/1.c
+++ code/1.c
@@ -21,7 +21,8 @@ int main() {
     //выборка из N случайных величин
     int *sample = (int *)malloc(N * sizeof(int));
 
-    //массив вероятностей для каждого значения k от 0 до m-1
+    //массив теоретических вероятностей 
+    //для каждого значения k от 0 до m-1
     double *probabilities = (double *)malloc(m * sizeof(double));
 
     for (int k = 0; k < m; k++) {
@@ -35,6 +36,7 @@ int main() {
 
     srand(time(NULL));
 
+    //генерация выборки
     for (int i = 0; i < N; i++) {
         float rnd = (float)rand()/RAND_MAX;
 
@@ -72,7 +74,7 @@ int main() {
     printf("\n");
     */
 
-    //вывод количества каждого значения в выборке
+    //количество каждого значения в выборке
     int *counts = (int *)calloc(m, sizeof(int));
     for (int i = 0; i < N; i++) {
         counts[sample[i]]++;
@@ -82,6 +84,36 @@ int main() {
     for (int k = 0; k < m; k++) {
         printf("Count(%d) = %d\n", k, counts[k]);
     }
+
+    //матожидание теоретичекое
+    double theoretical_mean = 0;
+    for (int k = 0; k < m; k++) {
+        theoretical_mean += k * probabilities[k];
+    }
+    printf("Theoretical mean = %f\n", theoretical_mean);
+
+    //матожидание выборки
+    double sample_mean = 0;
+    for (int i = 0; i < N; i++) {
+        sample_mean += sample[i];
+    }
+    sample_mean /= N;
+    printf("Sample mean = %f\n", sample_mean);
+
+    //дисперсия теоретическая
+    double theoretical_variance = 0;
+    for (int k = 0; k < m; k++) {
+        theoretical_variance += probabilities[k] * pow(k - theoretical_mean, 2);
+    }
+    printf("Theoretical variance = %f\n", theoretical_variance);
+
+    //дисперсия выборки
+    double sample_variance = 0;
+    for (int i = 0; i < N; i++) {
+        sample_variance += pow(sample[i] - sample_mean, 2);
+    }
+    sample_variance /= N;
+    printf("Sample variance = %f\n", sample_variance);
 
     //освобождение памяти
     free(sample);
